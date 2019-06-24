71c3753aa998ac0196610a586d6d44ac277a5824
Adnan Dervisevic <Adnan.Dervisevic@collectorbank.se>
2019-06-24 21:47:25 +0200

    Add build

---
+++ build/precache-manifest.26dd11c51dc405fcdb8dd0570ee3b9b2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1c6ac639d5dd983598acef4605b83cd",
+    "url": "/static/media/swedish-logo.d1c6ac63.svg"
+  },
+  {
+    "revision": "d782e37d323e288ac355",
+    "url": "/static/js/main.d782e37d.chunk.js"
+  },
+  {
+    "revision": "641a5f7199b81a126918c13fdff2e7f0",
+    "url": "/static/media/expand.641a5f71.svg"
+  },
+  {
+    "revision": "95d246ea1e516720cc11710ee8b4d740",
+    "url": "/static/media/collapse.95d246ea.svg"
+  },
+  {
+    "revision": "76869b2486d441fbab4ff2b1b43a6294",
+    "url": "/static/media/regular.76869b24.woff"
+  },
+  {
+    "revision": "c6301cf05b7c577f82da983d990f64e2",
+    "url": "/static/media/bold.c6301cf0.woff2"
+  },
+  {
+    "revision": "5408b07dbfb832b7e04bd894171ec772",
+    "url": "/static/media/semibold.5408b07d.woff2"
+  },
+  {
+    "revision": "e232955a55792780a591eeb2717487d0",
+    "url": "/static/media/regular.e232955a.woff2"
+  },
+  {
+    "revision": "75e74f3d603454f815f6108ae00f7296",
+    "url": "/static/media/semibold.75e74f3d.woff"
+  },
+  {
+    "revision": "783ba7276da61fb497701d320ac2df64",
+    "url": "/static/media/extrabold.783ba727.woff2"
+  },
+  {
+    "revision": "6747f143d1458fd96a4b6241b94928a4",
+    "url": "/static/media/extrabold.6747f143.woff"
+  },
+  {
+    "revision": "64136a35e4d6c83fcc94013f862966de",
+    "url": "/static/media/bold.64136a35.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "cb8702631241c63af5baa6040b0e5a76",
+    "url": "/static/media/house.cb870263.svg"
+  },
+  {
+    "revision": "51ddea4c65b75f2414db3c65f5a25e6e",
+    "url": "/static/media/documents.51ddea4c.svg"
+  },
+  {
+    "revision": "3b1d479abe000b3ee42b1e12ff4e6003",
+    "url": "/static/media/info-black.3b1d479a.svg"
+  },
+  {
+    "revision": "4c7872a01b9f8740c778d9007e4a8f49",
+    "url": "/static/media/success-black.4c7872a0.svg"
+  },
+  {
+    "revision": "7c1496bd818a14b7e18958dc661480c0",
+    "url": "/static/media/success.7c1496bd.svg"
+  },
+  {
+    "revision": "0dc47c4f462457e05880f154db71e326",
+    "url": "/static/media/warning-filled.0dc47c4f.svg"
+  },
+  {
+    "revision": "30a4cd10ac82589822fb5b603b498662",
+    "url": "/static/media/warning-red.30a4cd10.svg"
+  },
+  {
+    "revision": "0c84e5e032bd310208fa20c73183ac2a",
+    "url": "/static/media/warning.0c84e5e0.svg"
+  },
+  {
+    "revision": "6beacb766f764f8f65b7",
+    "url": "/static/js/2.6beacb76.chunk.js"
+  },
+  {
+    "revision": "6beacb766f764f8f65b7",
+    "url": "/static/css/2.34c7f873.chunk.css"
+  },
+  {
+    "revision": "6d36cfae7b45ed726695eed55a28397a",
+    "url": "/index.html"
+  }
+];
